4c4c73aef414281bdc5c2151097377314660a8c8
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-10-30 11:10:08 +0100

    fix: log

--- src/lib/socket-singleton.js
+++ src/lib/socket-singleton.js
@@ -1,6 +1,7 @@
 import socketIO from "socket.io-client";
 
 const apiKey = import.meta.env.VITE_SOCKET_URL;
+console.log(apiKey, 'apiKey');
 
 const socket = socketIO.connect(apiKey);
 export default socket;
