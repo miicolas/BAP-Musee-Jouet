4b0853aa531f5bdf3999f7c175e967ca067b3151
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-14 08:55:46 +0100

    chore: sounds import & components

--- src/routes/avatar.jsx
+++ src/routes/avatar.jsx
@@ -1,14 +1,14 @@
-import {Link} from "react-router-dom";
-import {Canvas} from "@react-three/fiber";
+import { Link } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
 import Experience from "../components/models/experience";
-import {useEffect, useState, useRef} from "react";
+import { useEffect, useState } from "react";
 import Layout from "../layout";
-import {questions} from "../lib/utils";
+import TypeAnimationEffect from "../components/type-animation";
+import SoundManager from "../components/sound-manager";
+import { questions } from "../lib/utils";
 import socket from "../lib/socket-singleton";
-import {TypeAnimation} from 'react-type-animation';
-import Bizon from "../assets/sounds/bizon.mp3";
-import Meow from "../assets/sounds/meow.mp3";
-import Minecraft from "../assets/sounds/minecraft.mp3";
+import { Meow, Bizon, Minecraft } from "../lib/sounds-import.js";
+
 
 export default function Avatar() {
     const [response, setResponse] = useState(null);
@@ -16,10 +16,8 @@ export default function Avatar() {
     const [animateKey, setAnimateKey] = useState(0);
     const [avatarID, setAvatarID] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(true);
-    const [setSoundAutorized] = useState(false);
+    const [soundAutorized, setSoundAutorized] = useState(false);
     const [sound, setSound] = useState(null);
-    const [currentAudio, setCurrentAudio] = useState(null);
-    const audioRef = useRef(null);
 
     const handleContextMenu = (event) => {
         event.preventDefault();
@@ -70,20 +68,6 @@ export default function Avatar() {
     }, [socketResponse, avatarID]);
 
     useEffect(() => {
-        if (response && buttonRef.current) {
-            buttonRef.current.click();
-        }
-        if (response) {
-            if (currentAudio) {
-                currentAudio.pause();
-                currentAudio.currentTime = 0;
-            }
-            const audio = new Audio(sound);
-            setCurrentAudio(audio);
-            audio.play().catch(error => {
-                console.error("Error playing audio:", error);
-            });
-        }
         setAnimateKey(prevKey => prevKey + 1);
     }, [response]);
 
@@ -99,37 +83,16 @@ export default function Avatar() {
                     Retour
                 </button>
             </Link>
-            <div
-                className="absolute bottom-0 left-1/2 -translate-x-1/2 h-20 max-w-sm bg-amber-700 z-50 flex justify-center items-center p-6">
-                {
-                    response ? (
-                        <TypeAnimation
-                            key={animateKey}
-                            sequence={[
-                                response,
-                                1000,
-                            ]}
-                            wrapper="span"
-                            speed={50}
-                            style={{fontSize: '1em', display: 'inline-block'}}
-                            repeat={0}
-                        />
-                    ) : (
-                        <p>Aucune question n'a été répondu à pour l'instant.</p>
-                    )
-                }
-            </div>
-            <Canvas shadows camera={{position: [0, 2, 5], fov: 75}} style={{width: "100vw", height: "100vh"}}
-                    className="r3f-canvas">
-                <Experience avatarID={avatarID}/>
+            <TypeAnimationEffect response={response} animateKey={animateKey} />
+            <Canvas shadows camera={{ position: [0, 2, 5], fov: 75 }} style={{ width: "100vw", height: "100vh" }} className="r3f-canvas">
+                <Experience avatarID={avatarID} />
             </Canvas>
-
-            <audio ref={audioRef}/>
+            <SoundManager sound={sound} play={!!response} />
             {isModalOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
                     <div className="bg-white rounded-lg p-4 shadow-lg">
                         <h2 className="text-2xl font-bold mb-4">Bienvenue dans le jeu de rôle!</h2>
-                        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700" onClick={() => handleAutorization()}>Ok</button>
+                        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700" onClick={handleAutorization}>Ok</button>
                     </div>
                 </div>
             )}
