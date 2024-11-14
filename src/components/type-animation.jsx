04d37da9e25749f0a0ab1c71dabeb89f102bc12f
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-14 08:56:29 +0100

    chore: animation typing components

---
+++ src/components/type-animation.jsx
@@ -0,0 +1,28 @@
+import {TypeAnimation} from 'react-type-animation';
+
+export default function TypeAnimationEffect({response, animateKey}) {
+
+
+    return (
+        <div
+            className="absolute bottom-0 left-1/2 -translate-x-1/2 h-20 max-w-sm bg-amber-700 z-50 flex justify-center items-center p-6">
+            {
+                response ? (
+                    <TypeAnimation
+                        key={animateKey}
+                        sequence={[
+                            response,
+                            1000,
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{fontSize: '1em', display: 'inline-block'}}
+                        repeat={0}
+                    />
+                ) : (
+                    <p>Aucune question n'a été répondu à pour l'instant.</p>
+                )
+            }
+        </div>
+    );
+}
