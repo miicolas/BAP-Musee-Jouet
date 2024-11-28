3f39e098781710b9a1cd9434c67e6a6049695549
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-28 11:38:06 +0100

    feat: modal star

--- src/components/notices-stars.jsx
+++ src/components/notices-stars.jsx
@@ -1,32 +1,19 @@
-import {motion} from "motion/react"
-import {Star} from "lucide-react";
-import {useState, useEffect} from "react";
+import { motion } from "motion/react";
+import { Star } from "lucide-react";
+import { useState } from "react";
 
-export default function NoticesStars() {
-
+export default function NoticesStars({ openModal }) {
     const [isLiked, setIsLiked] = useState(false);
-    
+
     const handleLike = () => {
-        const response = fetch(`${import.meta.env.VITE_SOCKET_URL}/api/likes`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
-        response.then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setIsLiked(true);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        openModal();
+        setIsLiked(true);
     }
 
     return (
         <motion.button
             onClick={isLiked ? null : handleLike}
-            className="flex items-center px-4 py-2 justify-center min-w-max border-4 border-blue-700 absolute bottom-10 right-10"
+            className={`flex items-center px-4 py-2 justify-center min-w-max border-4 border-blue-700 absolute bottom-10 right-10 ${isLiked ? "bg-yellow-500" : "bg-blue-700"}`}
             animate={isLiked ? "liked" : "unliked"} >
             {!isLiked ? (
                 <Star size={24} className="text-yellow-500 fill-yellow-500" />
@@ -44,15 +31,14 @@ export default function NoticesStars() {
     );
 }
 
-
 const labelTextVariants = {
-    unliked: { x: 24},
+    unliked: { x: 24 },
     liked: { x: -5 }
 };
 
 const successTextVariants = {
     unliked: { opacity: 0, transition: { duration: 0.25 } },
-    liked: { opacity: 1, }
+    liked: { opacity: 1 }
 };
 
 const likedTransition = {
