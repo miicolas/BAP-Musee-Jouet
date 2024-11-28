c07d1ecbcfe1e7e1d69342a5fab6989238f34380
AB <anasbounabat@gmail.com>
2024-11-28 11:51:08 +0100

    .

--- src/components/card-question.jsx
+++ src/components/card-question.jsx
@@ -10,13 +10,13 @@ export default function CardQuestion({person}) {
 
     const handleClick = () => {
         if (socket) {
-            socket.emit('question', person.id);
+            socket.emit('question',person.id);
         }
     };
 
     return (
         <div
-            className="p-4 m-4 transition-colors duration-300 bg-white border-2 border-gray-200 rounded-lg hover:border-blue-500 hover:bg-gray-200"
+            className="p-4 m-4 transition-colors duration-300 bg-white border-2 border-orange-300 rounded-[15px]  text-center shadow-[0_4px_10px_rgba(255,165,0,0.3)] mt-60"
             onClick={handleClick}
         >
             <p>{person.question}</p>
