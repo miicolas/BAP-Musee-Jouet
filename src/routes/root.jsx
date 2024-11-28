d3c795692bd7e48e0c063c1213131ed9d40a48fa
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-11-28 22:11:38 +0100

    fix: buttons transitions

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -9,12 +9,12 @@ export default function Home() {
         <h2 className="text-3xl font-semibold p-12">Choix de l'écran :</h2>
         <div className="flex flex-row items-center my-12">
           <Link to="/avatar">
-            <button className="px-4 py-2 font-bold text-white bg-yellow-600 rounded-full hover:bg-yellow-700 mx-8 w-64 h-20 text-lg shadow-md shadow-yellow-700 hover:shadow-amber-900">
+            <button className="px-4 py-2 font-bold text-white bg-yellow-600 rounded-full hover:bg-yellow-700 mx-8 w-64 h-20 text-lg shadow-md shadow-yellow-700 hover:shadow-amber-900 transition-all duration-500">
               Affichage de l'avatar
             </button>
           </Link>
           <Link to="/choice-avatar">
-            <button className="px-4 py-2 font-bold text-white bg-yellow-600 rounded-full hover:bg-yellow-700 mx-8 w-64 h-20 text-lg shadow-md shadow-yellow-700 hover:shadow-amber-900">
+            <button className="px-4 py-2 font-bold text-white bg-yellow-600 rounded-full hover:bg-yellow-700 mx-8 w-64 h-20 text-lg shadow-md shadow-yellow-700 hover:shadow-amber-900 transition-all duration-500">
               Choix de l'avatar + Questions
             </button>
           </Link>
