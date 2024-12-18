36b5807da89cbcd1d813c627d10c65834d971a05
Maxence Rault <raultmaxence05@gmail.com>
2024-12-18 12:09:15 +0100

    Ajout logo

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,12 +1,20 @@
+import LOGO from './assets/images/logo.png';
+
 export default function Layout({ children, className, style }) {
-    return (
+  return (
+    <div>
       <div
         className={`min-h-screen mx-auto overflow-hidden sm:overflow-visible bg-background-color z-0 ${className}`}
         style={style}
       >
         {children}
       </div>
-    );
-  }
-
-  
+      <div className="absolute bottom-0 left-0 z-10 p-4">
+        <img src={LOGO} alt="logo" className="w-20 h-20" />
+          
+        
+          
+      </div>
+    </div>
+  );
+}
